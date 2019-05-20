d6dbfe56229668d073e17eeef9ad309e96a3d921
Root USO User <student@stud.acs.upb.ro>
2019-05-20 08:36:04 +0300

    implemented graphic task

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest2/PracticalTest2MainActivity.java
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.eim.practicaltest2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PracticalTest2MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test2_main);
+    }
+}
